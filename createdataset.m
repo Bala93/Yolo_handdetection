b595d6d827c04301ea00bc550f1e752887bdfa7f
Balamurali <balamuralim.1993@gmail.com>
2017-10-12 16:50:43 +0530

    Create yolo label format from oxford hand dataset

---
+++ createdataset.m
@@ -0,0 +1,50 @@
+function createdataset
+
+
+uf = dir('training_data/images/*.jpg');
+
+% size(uf)
+
+for i = 1:length(uf)
+    
+    
+    dot = strfind(uf(i).name,'.');
+    imname = uf(i).name(1:dot-1);
+    load(['training_data/annotations/' imname '.mat']);
+    im = imread(['training_data/images/' uf(i).name]);
+    annot_filename = ['training_data/yoloannotation/',imname,'.txt'];
+%     annot_filename
+    ann_data = fopen(annot_filename,'w');
+    [imgw,imgh] = size(im);
+%     imshow(im);
+    for j = 1:length(boxes)
+        box = boxes{j};
+        
+        %line([box.a(2) box.b(2)]',[box.a(1) box.b(1)]','LineWidth',3,'Color','y');
+%         line([box.b(2) box.c(2) box.d(2) box.a(2)]',[box.b(1) box.c(1) box.d(1) box.a(1)]','LineWidth',3,'Color','r');
+        x_ = [box.b(2) box.c(2) box.d(2) box.a(2)];
+        y_ = [box.b(1) box.c(1) box.d(1) box.a(1)];
+        
+        x1  = min(x_);
+        y1  = min(y_);
+        x2  = max(x_);
+        y2  = max(y_);
+        
+        bound_w = x2 - x1;
+        bound_h = y2 - y1;
+        
+%         line([box.b(2) box.c(2) box.d(2) box.a(2)]',[box.b(1) box.c(1) box.d(1) box.a(1)]','LineWidth',3,'Color','r');
+%         line([box.b(2) box.c(2) box.d(2) box.a(2)]',[box.b(1) box.c(1) box.d(1) box.a(1)]','LineWidth',3,'Color','r');
+%         rectangle('Position',[x1,y1,bound_w,bound_h],'LineWidth',3,'EdgeColor','r');
+        lable_text =[num2str(1) ' ' num2str(x1/imgw) ' ' num2str(y1/imgh) ' ' num2str(bound_w/imgw) ' ' num2str(bound_h/imgh)];
+        lable_text = [lable_text '\n'];
+        fprintf(ann_data,lable_text);
+%         break
+    end
+    fclose(ann_data);
+    
+%     imshow(im)
+%     disp('Press any key to move onto the next image');pause;
+    
+%     break
+end
